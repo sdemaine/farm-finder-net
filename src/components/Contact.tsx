7e5f84a4e198b0aae9f49903e3b3dd700cf5007a
Stephen Demaine <stephendemaine@gmail.com>
2024-09-26 22:05:51 -0400

    added routing and some test pages

---
+++ src/components/Contact.tsx
@@ -0,0 +1,13 @@
+
+type Props = {}
+
+function Contact({}: Props) {
+    return (
+        <div>
+          <h2>Contact Page</h2>
+          <p>Feel free to reach out!</p>
+        </div>
+    )
+}
+
+export default Contact
