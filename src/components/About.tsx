23052918a484f0beb3b63d7e8f174453121d2b15
Stephen Demaine <stephendemaine@gmail.com>
2024-09-26 22:12:02 -0400

    Add routing (#4)
    * a simple change to test workflow
    
    * fixed syntax error in preview.yaml file
    
    * fixed workflow files
    
    * removed bun
    
    * removed setCount
    
    * fixed production workflow
    
    * added routing and some test pages

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+type Props = {}
+
+export default function About({}: Props) {
+    return (
+        <div>
+          <h2>About Page</h2>
+          <p>This is the About page.</p>
+        </div>
+      );
+}
---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+type Props = {}
+
+export default function Home({ }: Props) {
+    return (
+        <div>
+            <h2>Home Page</h2>
+            <p>Welcome to the Home page!</p>
+        </div>
+    );
+}
