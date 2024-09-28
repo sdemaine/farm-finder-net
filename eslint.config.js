a1a095c875fd22e1ff1186c44efb966979c1873d
Stephen Demaine <stephendemaine@gmail.com>
2024-09-28 10:45:37 -0400

    cleaned up typescript compatibility issue

--- eslint.config.js
+++ eslint.config.js
@@ -22,10 +22,11 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': 'off',
+      // 'react-refresh/only-export-components': [
+      //   'warn',
+      //   { allowConstantExport: true },
+      // ],
     },
   },
 )
